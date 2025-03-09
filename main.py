d672bc04f8eee53647eda31ee99d30d90547fc84
Zamzam100 <kassimzamirah@gmail.com>
2025-03-09 09:48:09 -0700

    python tutorial

---
+++ main.py
@@ -0,0 +1,67 @@
+first_name ="Zamzam"
+print(first_name)
+
+#strings
+food ="nyama choma"
+print(f"I like {food}")
+
+#Integers
+age =20
+print(f"You are{age}years old")
+quantity =3
+print(f"You are buying{quantity}loaves of bread")
+Num_of_students =50
+print(f"This class has{Num_of_students}students")
+
+#float
+price =100.50
+print(f"The price of bread is ${price}")
+gpa =3.45
+print(f"My gpa is{gpa}")
+distance =34.5
+print(f"The distance between the two houses is{distance}km")
+
+#boolean
+is_student =True
+print(f"Are you a student?{is_student}")
+is_student =True
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
+for_sale =True
+if  for_sale:
+    print("For sale")
+else:
+    print("Not for sale")
+
+#Typecasting
+name ="Zamzam"
+age =20
+gpa =3.45
+is_student =True
+age =float(age)
+gpa =int(gpa)
+age =str(age)
+
+#Input()- prompts user to key in data and returns data as a string
+name =input("What is your name")
+print(f"Hello{name}")
+
+age =age+1
+print(f"Hello{name}")
+print("HAPPY BIRTYHDAY!")
+print(f"You are{age}years old")
+
+#Calculate the area of a rectangle
+
+length =input("Enter the length:")
+width =input("Enter the width:")
+area =length * width
+print("")
+
+
+
+
+
+
