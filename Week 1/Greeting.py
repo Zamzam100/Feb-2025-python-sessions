6468152b0c7cd37cd0c6464e8ad1fc76d704e5f1
Zamzam100 <kassimzamirah@gmail.com>
2025-03-09 18:38:02 -0700

    zamzam

---
+++ Week 1/Greeting.py
@@ -0,0 +1,3 @@
+greeting =str(input("Hello everyone!"))
+colour =str(input("What is your favourite colour?"))
+meal =str(input("What is your favourite meal?"))
---
+++ Week 1/index.py
@@ -0,0 +1,5 @@
+item =str(input("What item would you like to buy?:"))
+price =float(input("What is the price?:"))
+Quantity =int(input("How many do you want?:"))
+Total =float(price) * Quantity
+print(f"What is the total?:\n")
